bd3168b7d62176ed0ae24b8b2c9ed4a827320561
Kadaine Williams <kadainewilliams@gmail.com>
2024-01-07 22:04:29 -0500

    Initial Commit

---
+++ Server/Routes/adminRoute.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import con from '../utils/db.js'
+import jwt from 'jsonwebtoken'
+
+const router = express.Router()
+
+router.post('/adminlogin', (req, res) => {
+    //console.log(req.body) //Use to Validate that the submitted information is correct
+    const sql = 'SELECT * from admin WHERE email = ? AND password = ?'
+    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+        if (err) return res.json({ loginStatus: false, Error: 'Query error' })
+        if (result.length > 0) {
+            const email = result[0].email
+            const token = jwt.sign(
+                { role: 'admin', email: email },
+                'jwt_secret_key',
+                { expiresIn: '1d' })
+            res.cookie('token', token)
+            return res.json({ loginStatus: true })
+        } else {
+            return res.json({ loginStatus: false, Error: 'Wrong email or password'})
+        }
+    }) //Replaces the "?" in line 8 above with the request email and password
+})
+
+router.post('/add_category', (req, res) => {
+    const sql = 'INSERT INTO category (`name`) VALUES (?)'
+    con.query(sql, [req.body.category], (err, result) => {
+        if(err) return res.json({Status: false, Error: 'Query Error'})
+        return res.json({Status: true})
+    })
+})
+
+router.get('/category', (req, res) => {
+    const sql = 'SELECT * FROM category'
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: 'Query Error'})
+        return res.json({Status: true, Result: result})
+    })
+})
+
+export { router as adminRouter }
---
+++ Server/utils/db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql'
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employeems'
+})
+
+con.connect((err) => {
+    if(err) {
+        console.log('connection error')
+        console.log(err)
+    }else {
+        console.log('connected')
+    }
+})
+
+export default con
